9a883497d27675bebb84dc9c58ba0312da973e83
Rob Hoelz <rob@hoelz.ro>
2013-07-10 20:28:05 +0200

    Add some nitpicking/ideas
    This should be in my private repo for notes only

--- plu_lua.c
+++ plu_lua.c
@@ -15,6 +15,7 @@ plu_new_lua_state(pTHX)
 {
   lua_State *lua;
 
+  /* XXX the Lua state should probably use Perl's memory allocator */
   lua = luaL_newstate();
 
   /* C equivalent of Lua 'perl = {}' */
@@ -32,6 +33,7 @@ plu_get_lua_errmsg(pTHX)
   const char *str;
   size_t len;
   SV *errmsg;
+  /* XXX this should probably use luaL_checklstring */
   str = lua_tolstring(PLU_lua_int, -1, &len);
   errmsg = sv_2mortal(newSVpv(str, (STRLEN)len));
   lua_pop(PLU_lua_int, -1);
@@ -41,6 +43,7 @@ plu_get_lua_errmsg(pTHX)
 int
 plu_call_lua_func(pTHX_ const char *lua_func_name)
 {
+  /* XXX why not lua_getglobal? */
   lua_getfield(PLU_lua_int, LUA_GLOBALSINDEX, lua_func_name);
   return lua_pcall(PLU_lua_int, 0, 0, 0);
 }
@@ -50,6 +53,8 @@ int
 plu_compile_lua_block_or_croak(pTHX_ char *code, STRLEN len)
 {
   int status;
+  /* XXX can we include filename/line number information here? */
+  /* XXX IIRC, we would have to use a patched Lua to tell it to use Perl's line numbers */
   status = luaL_loadbuffer(PLU_lua_int, code, len, "_INLINED_LUA");
   switch (status) {
     case 0:
